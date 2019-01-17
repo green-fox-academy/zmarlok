1bc0ec6de6b2a90fb77f2dee87176bcd498ab0b7
Zoltan Marlok <m.zoltaan@gmail.com>
2019-01-17 14:10:36 +0100

    04_introduce_yourself

---
+++ week01/day4/04_introduce_yourself/src/_04_introduce_yourself.java
@@ -0,0 +1,17 @@
+public class _04_introduce_yourself {
+    public static void main(String[] args) {
+        // Write a program that prints a few details to the terminal window about you
+        // It should print each detail to a new line.
+        //  - Your name
+        //  - Your age
+        //  - Your height in meters (as a decimal fraction)
+        //
+        //  Example output:
+        //  John Doe
+        //  31
+        //  1.87
+        System.out.println("Zoltán Marlok");
+        System.out.println("29");
+        System.out.println("1.86");
+    }
+}
