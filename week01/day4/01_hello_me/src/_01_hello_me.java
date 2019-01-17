7733e6b69014e559e8a29a5942a9d7b4b233be93
Zoltan Marlok <m.zoltaan@gmail.com>
2019-01-17 12:59:50 +0100

    01_hello_me completed

---
+++ week01/day4/01_hello_me/src/_01_hello_me.java
@@ -0,0 +1,6 @@
+public class _01_hello_me {
+    public static void main(String[] args) {
+        // Modify this program to greet you instead of the World!
+        System.out.println("Hello, Me!");
+    }
+}
--- week01/day4/src/day4.java
+++
@@ -1,8 +0,0 @@
-public class day4 {
-    public static void main(String[] args) {
-                // Modify this program to greet you instead of the World!
-                System.out.println("Hello, Me!");
-            }
-}
-
-
