26147f03b4448c8fea5912721eaba6e367b6ce20
Zoltan Marlok <m.zoltaan@gmail.com>
2019-01-17 13:08:24 +0100

    03_hello_others completed

---
+++ week01/day4/03_hello_others/src/_03_hello_others.java
@@ -0,0 +1,13 @@
+public class _03_hello_others {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+        System.out.println("Hello Gabi!");
+        System.out.println("Hello Botond!");
+        System.out.println("Hello Dezső!");
+    }
+}
