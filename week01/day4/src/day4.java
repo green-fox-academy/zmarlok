618a975314145c8d62db1f3b5d78d10cd07268bd
Zoltan Marlok <m.zoltaan@gmail.com>
2019-01-17 12:10:04 +0100

    1 excersise: Hello, Me! done

---
+++ week01/day4/src/day4.java
@@ -0,0 +1,8 @@
+public class day4 {
+    public static void main(String[] args) {
+                // Modify this program to greet you instead of the World!
+                System.out.println("Hello, Me!");
+            }
+}
+
+
