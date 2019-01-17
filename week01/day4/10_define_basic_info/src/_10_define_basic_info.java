d54f3d3a25ef48df71b4a5def0025f8b5421b48f
Zoltan Marlok <m.zoltaan@gmail.com>
2019-01-17 15:23:18 +0100

    10_define_basic_info completed

---
+++ week01/day4/10_define_basic_info/src/_10_define_basic_info.java
@@ -0,0 +1,18 @@
+public class _10_define_basic_info {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        String myName = "Zoli";
+        // Your age as an integer
+        int myAge = 29;
+        // Your height in meters as a double
+        double myHeight = 1.87;
+        // Whether you are married or not as a boolean
+        boolean maritalStatus = false;
+
+        System.out.println("Hi! My name is " + myName);
+        System.out.println("I am " + myAge + " years old");
+        System.out.println("I am " + myHeight + " meters tall");
+        System.out.println("Zoli is married. This statement is " + maritalStatus);
+    }
+}
