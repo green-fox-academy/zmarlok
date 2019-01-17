d9e8619aa35c0362eef1dea4b122f60c3c1fd041
Zoltan Marlok <m.zoltaan@gmail.com>
2019-01-17 14:56:11 +0100

    06_fav_number completed

---
+++ week01/day4/07_fav_numbers/src/_07_fav_number.java
@@ -0,0 +1,8 @@
+public class _07_fav_number {
+    public static void main(String[] args) {
+        int favoriteNumber = 5;
+        System.out.println("My favourite number is: " + favoriteNumber);
+        // Store your favorite number in a variable (as a number)
+        // And print it like this: "My favorite number is: 8"
+    }
+}
