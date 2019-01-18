4eb3dd71aec1ed4a024c40b3c0616d2281a4163c
Zoltan Marlok <m.zoltaan@gmail.com>
2019-01-18 10:57:53 +0100

    19_one_two_a_lot completed

---
+++ week01/day5/19_one_two_a_lot/src/_19_one_two_a_lot.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class _19_one_two_a_lot {
+    public static void main(String[] args) {
+
+        // Write a program that reads a number form the standard input,
+        // If the number is zero or smaller it should print: Not enough
+        // If the number is one it should print: One
+        // If the number is two it should print: Two
+        // If the number is more than two it should print: A lot
+
+        System.out.println("Please, enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        int userInput1 = Integer.parseInt(scanner.nextLine());
+
+        if (userInput1 <= 0) {
+            System.out.println("Not enough");
+        } else if (userInput1 == 1) {
+            System.out.println("One");
+        } else if (userInput1 == 2) {
+            System.out.println("Two");
+        } else if (userInput1 > 2) {
+            System.out.println("A lot");
+        }
+    }
+}
