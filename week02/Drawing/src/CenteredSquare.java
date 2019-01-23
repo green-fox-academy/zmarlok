9b1b52b512fda1469e1097aa02f321f7f4a7587e
Zoltan Marlok <m.zoltaan@gmail.com>
2019-01-23 14:37:44 +0100

    CenteredSquare completed

---
+++ week02/Drawing/src/CenteredSquare.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class CenteredSquare {
+
+    public static void mainDraw(Graphics graphics) {
+        // Draw a green 10x10 square to the canvas' center.
+
+        drawRectCenter(100,100, graphics);
+
+    }
+
+    public static void drawRectCenter(int a, int b, Graphics graphics){
+
+        graphics.drawRect(160 - a/2, 160 - a/2, a, b);
+    }
+
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
