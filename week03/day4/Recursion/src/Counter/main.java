d29c9158cc78177966b450a95181d98fe08e93c2
Zoltan Marlok <m.zoltaan@gmail.com>
2019-01-31 11:03:11 +0100

    Counter completed

---
+++ week03/day4/Recursion/src/Counter/main.java
@@ -0,0 +1,18 @@
+// Write a recursive function that takes one parameter: n and counts down from n.
+
+package Counter;
+
+public class main {
+
+    public static void main(String[] args) {
+        countToZero(10);
+    }
+
+    public static void countToZero(int n){
+        if(n > 0){
+            System.out.println(n);
+            countToZero(n-1);
+
+        } return;
+    }
+}
