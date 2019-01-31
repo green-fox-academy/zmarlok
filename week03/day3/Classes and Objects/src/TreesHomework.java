a0b96a1d5b39283a265c875a35916eff91183bf6
Zoltan Marlok <m.zoltaan@gmail.com>
2019-01-31 13:02:03 +0100

    TreesHomework draft

---
+++ week03/day3/Classes and Objects/src/TreesHomework.java
@@ -0,0 +1,46 @@
+//Create 5 trees
+//Store the data of them in variables in your program
+//for every tree the program should store its'
+//type
+//leaf color
+//age
+//sex
+//you can use just variables, or lists and/or maps
+
+import java.util.HashMap;
+
+/*public class TreesHomework {
+
+    public static void main(String[] args) {
+
+
+        String typeOfTree1 = Oak;
+        String leafColor1 = green;
+        int ageOfTree1 = 10;
+        String sexOfTree1 = female;
+
+        String typeOfTree2 = Oak;
+        String leafColor2 = green;
+        int ageOfTree2 = 10;
+        String sexOfTree2 = female;
+
+        String typeOfTree3 = Oak;
+        String leafColor3 = green;
+        int ageOfTree3 = 10;
+        String sexOfTree3 = female;
+
+        String typeOfTree4 = Oak;
+        String leafColor4 = green;
+        int ageOfTree4 = 10;
+        String sexOfTree4 = female;
+
+        String typeOfTree5 = Oak;
+        String leafColor5 = green;
+        int ageOfTree5 = 10;
+        String sexOfTree5 = female;
+
+    }
+
+}
+
+*/
