87744d16cbae6fcd5ec58ed56645885d39608a5f
Zoltan Marlok <m.zoltaan@gmail.com>
2019-01-31 20:13:23 +0100

    GuessTheNumber completed

---
+++ week01/day5/Expression and Control Flow/src/GuessTheNumber.java
@@ -0,0 +1,30 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+import java.util.Scanner;
+public class GuessTheNumber {
+
+
+    public static void main(String[] args) {
+        System.out.println("Guess my number: ");
+        Scanner scanner = new Scanner(System.in);
+        int userNumber = scanner.nextInt();
+        int myNumber = 8;
+
+        while (userNumber != myNumber) {
+             if (userNumber < myNumber) {
+                System.out.println("The entered number is lower");
+                  System.out.println("Guess my number: ");
+                  userNumber = scanner.nextInt();
+
+            } else if (userNumber > myNumber) {
+                System.out.println("The entered number is higher");
+                  System.out.println("Guess my number: ");
+                  userNumber = scanner.nextInt();
+              }
+        }
+        System.out.println("You found the number: " + myNumber);
+    }
+}
