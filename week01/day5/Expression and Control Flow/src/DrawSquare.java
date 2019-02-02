a9b9b34fccab7a0708f15c0712cc33d1d9c24252
Zoltan Marlok <m.zoltaan@gmail.com>
2019-02-02 13:28:47 +0100

    DrawSquare completed

---
+++ week01/day5/Expression and Control Flow/src/DrawSquare.java
@@ -0,0 +1,38 @@
+// Write a program that reads a number from the standard input, then draws a
+// square like this:
+//
+// %%%%%%
+// %    %
+// %    %
+// %    %
+// %    %
+// %%%%%%
+//
+// The square should have as many lines as the number was
+import java.util.Scanner;
+
+public class DrawSquare {
+    public static void main(String[] args) {
+
+        System.out.println("Please enter a number:");
+        Scanner scanner = new Scanner(System.in);
+        int lines = scanner.nextInt();
+
+        for (int i = 0; i < lines; i++) {
+            for (int j = 0; j < lines; j++) {
+                if (i == 0 | i == lines-1) {
+                    System.out.print('%');
+                }
+                else {
+                    if (j == 0 | j == lines - 1) {
+                        System.out.print('%');
+                    }
+                    else {
+                        System.out.print(' ');
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
