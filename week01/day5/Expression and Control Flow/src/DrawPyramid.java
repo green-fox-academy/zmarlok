cbd1a43a1c6613fa5c53abffaccbe0c1cc3be4db
Zoltan Marlok <m.zoltaan@gmail.com>
2019-02-02 12:56:30 +0100

    Draw Pyramid completed

--- week01/day5/Expression and Control Flow/src/DrawPyramid.java
+++ week01/day5/Expression and Control Flow/src/DrawPyramid.java
@@ -1,12 +1,13 @@
 // Write a program that reads a number from the standard input, then draws a
-// triangle like this:
+// pyramid like this:
 //
-// *
-// **
-// ***
-// ****
 //
-// The triangle should have as many lines as the number was
+//    *
+//   ***
+//  *****
+// *******
+//
+// The pyramid should have as many lines as the number was
 
 import java.util.Scanner;
 
@@ -14,16 +15,31 @@ public class DrawPyramid {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("How many lines high will be the triangle?");
+        System.out.println("How many lines high will be the pyramid?");
         int lines = scanner.nextInt();
 
-        for (int i = 1; i <= lines; i++)
+       for (int i = 0; i < lines; i++)
         {
-            for (int j = 1; j <= i; j++)
-            {
-                System.out.print("*");
+            for (int j = 0; j < lines-i; j++) {
+                System.out.print(" ");
             }
-            System.out.println("");
+            for (int k = 0; k <= i; k++) {
+                System.out.print("* ");
+            }
+        System.out.println();
         }
     }
 }
+//          for (int i = 0; i < 5; i++) {
+//            for (int j = 0; j < 5 - i; j++) {
+//                System.out.print(" ");
+//            }
+//            for (int k = 0; k <= i; k++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//
+//
+//Read more: http://www.java67.com/2015/10/how-to-print-pyramid-pattern-in-java-example.html#ixzz5eNF0kpd0
+
