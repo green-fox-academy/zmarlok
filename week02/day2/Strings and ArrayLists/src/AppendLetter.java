a1599ed4b1fe61230832a88ba2a4d5763c95fe63
Zoltan Marlok <m.zoltaan@gmail.com>
2019-02-03 16:57:20 +0100

    AppendLetter some corrections

--- week02/day2/Strings and ArrayLists/src/AppendLetter.java
+++ week02/day2/Strings and ArrayLists/src/AppendLetter.java
@@ -1,23 +1,22 @@
-/* imrt java.util.ArrayList;
-import java.util.List;
-
+import java.util.*;
 
 public class AppendLetter{
-    public static void main(String... args){
+    public static void main(String... args) {
         List<String> far = Arrays.asList("bo", "anacond", "koal", "pand", "zebr");
-
-
-        }
-
-        }
         // Create a method called "appendA()" that adds "a" to every string in the "far" list.
         // The parameter should be a list.
+        appendA(far);
 
+    }
+    public static void appendA(List<String> listWithoutA){
 
+        ArrayList<String> listWithA = new ArrayList<>();
 
-       // System.out.println(appendA(far));
+        for (int i = 0; i < listWithoutA.size() ; i++) {
+            listWithA.add(listWithoutA.get(i) + "a");
+        }
+        System.out.println(listWithA);
     }
 }
 
 // The output should be: "boa", "anaconda", "koala", "panda", "zebra"
-*/
