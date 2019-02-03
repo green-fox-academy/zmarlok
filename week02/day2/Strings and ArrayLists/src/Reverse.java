510b3f19967ad551676a9bd76e50b6cdc5f49f9a
Zoltan Marlok <m.zoltaan@gmail.com>
2019-02-03 14:48:59 +0100

    Reverse completed

---
+++ week02/day2/Strings and ArrayLists/src/Reverse.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Reverse {
+    public static void main(String... args) {
+        String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+        // Create a method that can reverse a String, which is passed as the parameter
+        // Use it on this reversed string to check it!
+        // Try to solve this using charAt() first, and optionally anything else after.
+
+        //ArrayList<Character> reversedString = new ArrayList<>();
+        ArrayList<String> revertedSentence = new ArrayList<>();
+
+        for (int i = reversed.length() - 1; i >= 0; i--) {
+            //reversedString.add(reversed.charAt(i));
+
+            revertedSentence.add(Character.toString(reversed.charAt(i)));
+        }
+
+        for (int i = 0; i < revertedSentence.size(); i++) {
+            System.out.print(revertedSentence.get(i));
+        }
+
+    }
+}
