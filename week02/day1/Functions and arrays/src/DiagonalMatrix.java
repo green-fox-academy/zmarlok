cf4044b8354114113ae88ae106833c3e69cbb0b3
Zoltan Marlok <m.zoltaan@gmail.com>
2019-02-03 11:23:40 +0100

    DiagonalMatrix completed

---
+++ week02/day1/Functions and arrays/src/DiagonalMatrix.java
@@ -0,0 +1,29 @@
+// - Create (dynamically) a two dimensional array
+//   with the following matrix. Use a loop!
+//
+//   1 0 0 0
+//   0 1 0 0
+//   0 0 1 0
+//   0 0 0 1
+//
+// - Print this two dimensional array to the output
+
+public class DiagonalMatrix {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = new int[4][4];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i==j) {
+                    matrix[i][j] = 1;
+                } else {
+                    matrix[i][j] = 0;
+                }
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
