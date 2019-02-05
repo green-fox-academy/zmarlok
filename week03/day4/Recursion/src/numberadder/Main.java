8df19c789642f7d7c1981068b3aeb2087d44c441
Zoltan Marlok <m.zoltaan@gmail.com>
2019-02-05 12:26:04 +0100

    NumberAdder updated

--- week03/day4/Recursion/src/numberadder/Main.java
+++ week03/day4/Recursion/src/numberadder/Main.java
@@ -5,14 +5,16 @@ package numberadder;
 public class Main {
 
     public static void main(String[] args) {
-            addOneUntilThisNumber(10);
-        }
 
-        public static void addOneUntilThisNumber(int n){
-            if(n > 0){
-                addOneUntilThisNumber(n-1);
-                System.out.println(n);
+        int result = addOneUntilThisNumber(3);
+        System.out.println(result);
+    }
 
-            } return;
+    public static int addOneUntilThisNumber(int n){
+        if(n > 0){
+            return n+addOneUntilThisNumber(n-1);
+            //addOneUntilThisNumber(n-1);
         }
+        return 0;
+    }
 }
