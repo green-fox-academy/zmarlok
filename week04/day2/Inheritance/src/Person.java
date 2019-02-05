3007163044335e11a0676a1f377aa177656e10b8
Zoltan Marlok <m.zoltaan@gmail.com>
2019-02-05 12:22:28 +0100

    Inheritance started

---
+++ week04/day2/Inheritance/src/Person.java
@@ -0,0 +1,22 @@
+//Create a Person class with the following fields:
+//
+//    name: the name of the person
+//    age: the age of the person (whole number)
+//    gender: the gender of the person (male / female)
+//
+//And the following methods:
+//
+//    introduce(): prints out "Hi, I'm name, a age year old gender."
+//    getGoal(): prints out "My goal is: Live for the moment!"
+//
+//And the following constructors:
+//
+//    Person(name, age, gender)
+//    Person(): sets name to Jane Doe, age to 30, gender to female
+
+public class Person {
+
+    String name;
+    Int age;
+    String gender;
+}
