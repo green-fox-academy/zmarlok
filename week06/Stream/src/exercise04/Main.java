f49a21cc95749f4010b946b3ca94902b63e0cb2b
Zoltan Marlok <m.zoltaan@gmail.com>
2019-02-25 15:40:14 +0100

    Exercise 4 corrected for ODD numbers

--- week06/Stream/src/exercise04/Main.java
+++ week06/Stream/src/exercise04/Main.java
@@ -11,7 +11,7 @@ public class Main {
         List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
 
         numbers.stream()
-                .filter(number -> number %2 == 0)
+                .filter(number -> number %2 != 0)
                 .mapToInt(Integer::intValue).average()
                 .ifPresent(System.out::println);
     }
