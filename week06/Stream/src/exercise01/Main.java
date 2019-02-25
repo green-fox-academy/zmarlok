e3ad94936bc621a1f2df800c14e673c23830b06d
Zoltan Marlok <m.zoltaan@gmail.com>
2019-02-25 14:51:48 +0100

    Exercise01 format has been corrected

--- week06/Stream/src/exercise01/Main.java
+++ week06/Stream/src/exercise01/Main.java
@@ -7,10 +7,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Exercise 1:
+        // Write a Stream Expression to get the even numbers from the following list:
         List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
-        //Write a Stream Expression to get the even numbers from the following list:
+
         numbers.stream()
-                .filter(number -> number %2 ==0)
+                .filter(number -> number %2 == 0)
                 .forEach(System.out::println);
     }
 }
