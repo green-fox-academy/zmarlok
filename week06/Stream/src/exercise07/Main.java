e106e98c24b504ea7caeca632ed6c084de6e1383
Zoltan Marlok <m.zoltaan@gmail.com>
2019-02-26 12:01:57 +0100

    Ex 7 started

---
+++ week06/Stream/src/exercise07/Main.java
@@ -0,0 +1,16 @@
+package exercise07;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Write a Stream Expression to find the strings which starts with a given letter(as parameter), in the following list:
+        List<String> cities = Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS");
+
+       /* cities.stream()
+                .filter(city -> city.startsWith())*/
+
+    }
+}
