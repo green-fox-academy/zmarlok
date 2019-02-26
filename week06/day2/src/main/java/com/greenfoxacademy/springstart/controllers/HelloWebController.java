0729211534c01d12c9defb4ff9da8a9cecffa4b1
Zoltan Marlok <m.zoltaan@gmail.com>
2019-02-26 16:56:09 +0100

    Webgreet with random color and fontsize completed

--- week06/day2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week06/day2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
@@ -12,11 +13,23 @@ public class HelloWebController {
 
     AtomicLong webGreetCounter = new AtomicLong();
 
+    String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+            "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+            "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
     @RequestMapping("/web/greeting")
     public String greeting(Model model, @RequestParam String name) {
         webGreetCounter.getAndIncrement();
+        //String random = (fruits[new Random().nextInt(fruits.length)]);
+        String randomHello = (hellos[new Random().nextInt(hellos.length)]);
+        String randomColor = ((int)(Math.random()*256) + "," + (int)(Math.random()*256) + ", " + (int)(Math.random()*256));
+        int randomFontSize = (1 + (int)(Math.random()*72));
         model.addAttribute("name", name);
         model.addAttribute("id", webGreetCounter);
+        model.addAttribute("color", randomColor);
+        model.addAttribute("fontsize", randomFontSize);
+        model.addAttribute("hello", randomHello);
         return "greeting";
     }
 }
