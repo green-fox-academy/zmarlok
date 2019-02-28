b19becf83cf681ec49b29f0d2ace24dc057c5c50
Zoltan Marlok <m.zoltaan@gmail.com>
2019-02-28 16:35:25 +0100

    Webshop application HTML part 80% completed

---
+++ week06/day4/basicwebshop/src/main/java/com/greenfoxacademy/basicwebshop/BasicwebshopApplication.java
@@ -0,0 +1,22 @@
+package com.greenfoxacademy.basicwebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class BasicwebshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicwebshopApplication.class, args);
+    }
+
+    @RequestMapping
+    @ResponseBody
+    public String hello (){
+        return "Hello WEBSHOP";
+    }
+}
---
+++ week06/day4/basicwebshop/src/main/java/com/greenfoxacademy/basicwebshop/controllers/WebshopController.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy.basicwebshop.controllers;
+
+import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class WebshopController {
+
+
+   /* Create a data structure for storing the ShopItems
+    Have a predefined list of items in your controller
+    you can initialize the list and add some shop items in the constructor of the controller */
+}
