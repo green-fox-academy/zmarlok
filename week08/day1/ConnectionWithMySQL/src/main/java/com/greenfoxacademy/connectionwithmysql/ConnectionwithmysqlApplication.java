cba142d8eb37cb4c05832676975874db700ead49
Zoltan Marlok <m.zoltaan@gmail.com>
2019-03-11 16:14:02 +0100

    Connection with MYSQL updated

--- week08/day1/ConnectionWithMySQL/src/main/java/com/greenfoxacademy/connectionwithmysql/ConnectionwithmysqlApplication.java
+++ week08/day1/ConnectionWithMySQL/src/main/java/com/greenfoxacademy/connectionwithmysql/ConnectionwithmysqlApplication.java
@@ -1,13 +1,29 @@
 package com.greenfoxacademy.connectionwithmysql;
 
+import com.greenfoxacademy.connectionwithmysql.models.Todo;
+import com.greenfoxacademy.connectionwithmysql.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConnectionwithmysqlApplication {
+public class ConnectionwithmysqlApplication implements CommandLineRunner {
+    private TodoRepository todoRepository;
+
+    @Autowired
+    public ConnectionwithmysqlApplication(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ConnectionwithmysqlApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        todoRepository.save(new Todo("I have to learn Object Relational Mapping"));
+        todoRepository.save(new Todo("I have to understand everything"));
+        todoRepository.save(new Todo("I have to learn CommandLineRunner"));
+    }
 }
