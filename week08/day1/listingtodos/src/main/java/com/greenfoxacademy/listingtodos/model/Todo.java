23fec4f05e0338b58bcfb617ed0e8a24f220af10
Zoltan Marlok <m.zoltaan@gmail.com>
2019-03-11 12:00:00 +0100

    ListingTodos project initialized

---
+++ week08/day1/listingtodos/src/main/java/com/greenfoxacademy/listingtodos/model/Todo.java
@@ -0,0 +1,50 @@
+package com.greenfoxacademy.listingtodos.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Todo {
+
+    @Id
+    private long id;
+    private String title;
+    private boolean urgent;
+    private boolean done;
+
+    public Todo(long id, String title, boolean urgent, boolean done) {
+        this.id = id;
+        this.title = title;
+        this.urgent = urgent;
+        this.done = done;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isUrgent() {
+        return urgent;
+    }
+
+    public void setUrgent(boolean urgent) {
+        this.urgent = urgent;
+    }
+
+    public boolean isDone() {
+        this.done = false;
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
