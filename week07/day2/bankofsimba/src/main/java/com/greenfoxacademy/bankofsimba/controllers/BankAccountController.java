4ffa8aed14287ccae7803dea740fc9f9ea68cc3d
Zoltan Marlok <m.zoltaan@gmail.com>
2019-03-14 16:25:35 +0100

    Add new account form is working, without balance

--- week07/day2/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controllers/BankAccountController.java
+++ week07/day2/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controllers/BankAccountController.java
@@ -3,8 +3,7 @@ package com.greenfoxacademy.bankofsimba.controllers;
 import com.greenfoxacademy.bankofsimba.models.BankAccount;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +11,17 @@ import java.util.List;
 @Controller
 public class BankAccountController {
 
-    BankAccount simbaAccount = new BankAccount("Simba", 2000, "lion");
+    BankAccount simbaAccount = new BankAccount("Simba", 20000, "lion", true, true);
+    List<BankAccount> allAccount = new ArrayList<BankAccount>();
+
+    public BankAccountController() {
+        this.allAccount.add(new BankAccount("Simba", 2000, "lion", true, true));
+        this.allAccount.add(new BankAccount("Timon", 50, "rókamanguszta", false, true));
+        this.allAccount.add(new BankAccount("Pumba", 100, "wildhog", false, true));
+        this.allAccount.add(new BankAccount("Rafiki", 1500, "monkey", false, true));
+        this.allAccount.add(new BankAccount("Öreg Oroszlán", 200000, "lion", true, true));
+        this.allAccount.add(new BankAccount("Gonosz Állat", 10, "bird", false, false));
+    }
 
     @RequestMapping(value = "/show", method = RequestMethod.GET)
     public String showAccount(Model model) {
@@ -21,34 +30,27 @@ public class BankAccountController {
     }
 
     @RequestMapping(value = "/html_text", method = RequestMethod.GET)
-    public String showHTMLText(Model model){
+    public String showHTMLText(Model model) {
         model.addAttribute("someString", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
         return "test_string";
     }
 
+    @GetMapping("/all")
+    public String showAllAccount(Model model) {
+        model.addAttribute("allAccount", allAccount);
+        model.addAttribute("newBankAccount", new BankAccount());
+        return "all_accounts";
+    }
 
+    @PostMapping("/raise_balance")
+    public String raiseSelectedAccountsBalance(@RequestParam(value = "animalIndex") int index) {
+        allAccount.get(index).raiseBalance();
+        return "redirect:/all";
+    }
 
-
-    List<BankAccount> allAccount = new ArrayList<BankAccount>();
-    BankAccount timonAccount = new BankAccount("Timon", 50, "rókamanguszta");
-    BankAccount pumbaAccount = new BankAccount("Pumba", 100, "wildhog");
-    BankAccount rafikiAccount = new BankAccount("Rafiki", 1800, "monkey");
-    BankAccount oldLionAccount = new BankAccount("Öreg Oroszlán", 20000000, "lion");
-    BankAccount birdAccount = new BankAccount("Madár", 10, "bird");
-
-
-    @RequestMapping("/all")
-    public String showAllAccount(Model model){
-
-        allAccount.add(simbaAccount);
-        allAccount.add(timonAccount);
-        allAccount.add(pumbaAccount);
-        allAccount.add(rafikiAccount);
-        allAccount.add(oldLionAccount);
-        allAccount.add(birdAccount);
-
-        model.addAttribute("accountList", allAccount);
-
-        return "all";
+    @PostMapping("/add")
+    public String addBankAccount(@ModelAttribute BankAccount newBankAccount) {
+        allAccount.add(newBankAccount);
+        return "redirect:/all";
     }
 }
--- week07/day2/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/models/BankAccount.java
+++ week07/day2/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/models/BankAccount.java
@@ -5,23 +5,66 @@ public class BankAccount {
     private String name;
     private int balance;
     private String animalType;
+    private boolean isKing;
+    private boolean isGoodGuy;
 
+
+    public BankAccount(String name, int balance, String animalType, boolean isKing, boolean isGoodGuy) {
+        this.name = name;
+        this.balance = balance;
+        this.animalType = animalType;
+        this.isKing = isKing;
+        this.isGoodGuy = isGoodGuy;
+    }
+
+    public BankAccount(){
+    }
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getBalance() {
         return balance;
     }
 
+    public void setBalance(){
+        this.balance = balance;
+    }
+
+    public void raiseBalance() {
+        if (this.isKing()) {
+            this.balance += 100;
+        } else {
+            this.balance += 10;
+        }
+    }
+
     public String getAnimalType() {
         return animalType;
     }
 
-    public BankAccount(String name, int balance, String animalType) {
-        this.name = name;
-        this.balance = balance;
+    public void setAnimalType(String animalType) {
         this.animalType = animalType;
+    }
+
+    public boolean isKing(){
+        return isKing;
+    }
+
+    public void setKing(boolean king) {
+        isKing = king;
+    }
+
+    public boolean isGoodGuy() {
+        return isGoodGuy;
+    }
+
+    public void setGoodGuy(boolean goodGuy) {
+        isGoodGuy = goodGuy;
     }
 }
