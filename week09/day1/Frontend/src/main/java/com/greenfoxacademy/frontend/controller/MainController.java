c86e39eed4d0ae960b890504d4ff85434e79c037
Zoltan Marlok <m.zoltaan@gmail.com>
2019-03-20 16:09:55 +0100

    First exercise - Frontend completed

---
+++ week09/day1/Frontend/src/main/java/com/greenfoxacademy/frontend/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.frontend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String showIndexPage() {
+        return "index";
+    }
+}
+
